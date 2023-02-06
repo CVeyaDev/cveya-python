b058128b03824b7f0bf155d79dbcb782cf77e756
Iheb Ouelhazi <ouelhazi3iheb@gmail.com>
2023-02-06 20:23:33 +0100

    Merge ihebdev and main by hand

--- cv-gen-app/CvGenScript.py
+++ cv-gen-app/CvGenScript.py
@@ -1,7 +1,60 @@
-from docxtpl import DocxTemplate
-from docx2pdf import convert
 
-# create a document object
+from docxtpl import DocxTemplate
+
+# create a doc
+
+skills_and_experiences = [
+    {
+        "name": "skill1",
+        "tasks": ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet",
+            "Lorem ipsum dolor sit amet"],
+    },
+    {
+        "name": "skill2",
+        "tasks": ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet",
+            "Lorem ipsum dolor sit amet"],
+    },
+    {
+        "name": "skill3",
+        "tasks": ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet",
+            "Lorem ipsum dolor sit amet"],
+    },
+]
+
+employments = [
+    {
+        "title":"Prophet",
+        "period": "580-600",
+        "company":"quraish",
+        "city":"mecca",
+        "location":"medina",
+    },
+    {
+        "title": "depression",
+        "period" :"0000-2023",
+        "company":"asad",
+        "city":"earth",
+        "location":"bruh"
+    }
+    
+]
+
+education_history = [
+    {
+        "degree":"M.Sc computer science engineering",
+        "date":"2022",
+        "university": "institut superieur de zatla sud grombalia",
+        "country":"tunis",
+    },
+    {
+        "degree":"baccalaureat science experimental",
+        "date":"2021",
+        "university":"7abit nemshi medecien hhhhhhhhh",
+        "country":"jandouba",
+    }
+    
+]
+
 doc = DocxTemplate("Resume_Sample_Functional.docx")
 
 # create context dictionary
@@ -10,6 +63,10 @@ context = {
     "address": "janah",
     "phoneNumber": "69 696 969",
     "email": "sol3om@gmail.com",
+    "high_qualifications": ["text1","text2","text3"],
+    "skills_and_experiences": skills_and_experiences,
+    "employments": employments,
+    "education_history": education_history,
 }
 
 # render context into the document object
@@ -17,5 +74,4 @@ doc.render(context)
 
 # save the document object as a word file
 doc.save('cv-docx.docx')
-convert("cv-docx.docx", "cv-pdf.pdf")
 
